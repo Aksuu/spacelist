cfc4ab0ab383de70455508f3f2cd56280910d29f
Aksu <appsu-@hotmail.com>
2019-04-23 03:41:05 +0300

    Demo data, created index template

--- src/main/java/hh/swd20/spacelist/SpacelistApplication.java
+++ src/main/java/hh/swd20/spacelist/SpacelistApplication.java
@@ -1,13 +1,35 @@
 package hh.swd20.spacelist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.swd20.spacelist.domain.Item;
+import hh.swd20.spacelist.domain.ItemRepository;
 
 @SpringBootApplication
 public class SpacelistApplication {
+		private static final Logger log = LoggerFactory.getLogger(SpacelistApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpacelistApplication.class, args);
+	}
+	
+	@Bean 
+	public CommandLineRunner ItemsDemo(ItemRepository itemrepository) {
+		return (args) -> {
+			log.info("Some items");
+			
+			itemrepository.save(new Item("First header", "First Description"));
+			
+			log.info("Fetch all items");
+			for (Item item : itemrepository.findAll()) {
+				log.info(item.toString());
+			}
+		};
 	}
 
 }
